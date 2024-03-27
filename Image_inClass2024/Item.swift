b86a0707b9ea4ac7cfcc497e803114d16e8b43ac
Owen Hildreth <ohildreth@mines.edu>
2024-03-27 17:22:57 -0400

    Initial Commit

---
+++ Image_inClass2024/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Image_inClass2024
+//
+//  Created by Owen Hildreth on 3/27/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
